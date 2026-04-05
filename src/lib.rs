86ada4036d27ec2700b26eb44a237c82649054eb
woruo03 <woruo03@gmail.com>
2026-04-06 00:09:44 +0800

    封装扫雷游戏插件并接入主程序

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
-mod core;
-mod game;
-mod state;
-mod systems;
-mod ui;
-mod utils;
+use bevy::prelude::*;
+
+pub mod core;
+pub mod game;
+pub mod state;
+pub mod systems;
+pub mod ui;
+pub mod utils;
+
+pub struct MinesweeperPlugin;
+
+impl Plugin for MinesweeperPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(core::resources::GameConfig::new(22, 22, 10, 32.0))
+            .add_systems(Startup, systems::setup::setup)
+            .add_systems(Startup, systems::setup::restart_game);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use bevy::prelude::*;
 
+use minesweeper::MinesweeperPlugin;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(MinesweeperPlugin)
         .run();
 }
