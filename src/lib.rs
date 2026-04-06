2b99161fc8b7bd39c26eeb23b14d9a67762a8e54
woruo03 <woruo03@gmail.com>
2026-04-06 20:15:47 +0800

    实现鼠标点击交互与网格定位功能

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,9 @@ pub struct MinesweeperPlugin;
 impl Plugin for MinesweeperPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(core::resources::GameConfig::new(22, 22, 10, 32.0))
+            .insert_resource(core::resources::GridTable::new())
             .add_systems(Startup, systems::setup::setup)
-            .add_systems(Startup, systems::setup::restart_game);
+            .add_systems(Startup, systems::setup::restart_game)
+            .add_systems(Update, systems::input::handle_mouse_input);
     }
 }
--- src/systems/input.rs
+++ src/systems/input.rs
@@ -1,1 +1,31 @@
+use bevy::prelude::*;
+
+use crate::core::components::MainCamera;
+use crate::core::resources::GameConfig;
+use crate::core::resources::GridTable;
+use crate::utils::helpers::world_to_vec;
+
 // 输入处理
+
+pub fn handle_mouse_input(
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    windows: Query<&Window>,
+    cameras: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    game_config: Res<GameConfig>,
+    grid_table: ResMut<GridTable>,
+) {
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        let window = windows.single().unwrap();
+        let (camera, camera_transform) = cameras.single().unwrap();
+
+        if let Some(cursor_pos) = window.cursor_position()
+            && let Ok(world_pos) = camera.viewport_to_world_2d(camera_transform, cursor_pos)
+        {
+            let (i, j) = world_to_vec(world_pos.x, world_pos.y, &game_config);
+            println!("Clicked at: ({}, {})", i, j);
+            if let Some(entity) = grid_table.get_entity(i, j, &game_config) {
+                println!("Entity: {:?}", entity);
+            }
+        }
+    }
+}
